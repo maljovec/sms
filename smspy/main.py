332b557a1b401721e3b9e570933d14eb471e8e58
Dan Maljovec <maljovec@users.noreply.github.com>
2020-08-27 12:29:39 -0600

    Update main.py

--- smspy/main.py
+++ smspy/main.py
@@ -1,16 +1,16 @@
 from __future__ import absolute_import
 from twilio.rest import Client
-from .secret import account_sid, auth_token
+from .secret import account_sid, auth_token, to_number, from_number
 
 def send_message(args):
     client = Client(account_sid, auth_token)
     if args is not None and len(args):
         if type(args) != str:
-            client.messages.create(to="***REMOVED***", from_="***REMOVED***", body=' '.join(args))
+            client.messages.create(to=to_number, from_=from_number, body=' '.join(args))
         else:
-            client.messages.create(to="***REMOVED***", from_="***REMOVED***", body=args)
+            client.messages.create(to=to_number, from_=from_number, body=args)
     else:
-        client.messages.create(to="***REMOVED***", from_="***REMOVED***", body='Job is Done!')
+        client.messages.create(to=to_number, from_=from_number, body='Job is Done!')
 
 if __name__ == "__main__":
     from sys import argv
